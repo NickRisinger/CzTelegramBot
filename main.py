616286a11c8483a49f941b94227becbe7a95721d
NickRisinger <boozy044@gmail.com>
2025-05-29 19:54:51 +0300

    feat(bot): init bot

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import asyncio
-from prisma import Prisma
-from prisma.types import UserCreateInput
+import logging
+from aiogram import Bot, Dispatcher
+from aiogram.enums import ParseMode
+from aiogram.methods import DeleteWebhook
+from aiogram.client.default import DefaultBotProperties
+
+from config.config import Config
+
+from handlers.start import router as start_router
+
+logging.basicConfig(level=logging.INFO)
 
 async def main():
-    db = Prisma()
-    await db.connect()
+    bot = Bot(token=Config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+    dp = Dispatcher()
 
+    await bot(DeleteWebhook(drop_pending_updates=True))
+
+    dp.include_router(start_router)
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
     try:
-        user = await db.user.create(
-            data=UserCreateInput(tg_id=123456780)
-        )
-
-        print(f'Created user: {user.model_dump_json()}')
-    except Exception as e:
-        print(f'Error: {e}')
-
-    await db.disconnect()
-
-
-if __name__ == '__main__':
-    asyncio.run(main())
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
