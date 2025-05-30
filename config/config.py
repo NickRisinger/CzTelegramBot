d9f72af094f3d942565e88277527d526c1e23683
NickRisinger <boozy044@gmail.com>
2025-05-30 14:08:38 +0300

    feat(config): update config

--- config/config.py
+++ config/config.py
@@ -9,3 +9,5 @@ class Config:
     BOT_TOKEN: str = os.getenv('BOT_TOKEN')
     PATH_FILE_ALLOWED_GTINS: str = os.getenv('PATH_FILE_ALLOWED_GTINS')
     PATH_FILE_PRODUCTS: str = os.getenv('PATH_FILE_PRODUCTS')
+    BOT_SUPPORT_GROUP_ID: str = os.getenv('BOT_SUPPORT_GROUP_ID')
+    BOT_NOTIFY_GROUP_ID: str = os.getenv('BOT_NOTIFY_GROUP_ID')
