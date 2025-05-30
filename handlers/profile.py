872b4eb9fa85b85027950641dfd8d31c07a3d01a
NickRisinger <boozy044@gmail.com>
2025-05-30 10:17:14 +0300

    feat(profile): create profile handler

---
+++ handlers/profile.py
@@ -0,0 +1,33 @@
+from aiogram import Router, F
+from aiogram.types import Message
+# from database.database import get_user, create_user
+from prisma import Prisma
+from prisma.models import User
+
+from utils.utils import format_date
+
+router = Router()
+
+
+async def get_user(tg_id: int) -> User:
+    async with Prisma() as db:
+        return await db.user.find_first(
+            where={'tg_id': tg_id},
+            include={'codes': True}
+        )
+
+
+@router.message(F.text == 'Профиль')
+async def cmd_start(message: Message):
+    user = await get_user(message.chat.id)
+
+    profile_text = (
+        f"👤 <b>Профиль пользователя</b>\n\n"
+        f"🆔 ID: {message.from_user.id}\n"
+        f"👤 Username: @{message.from_user.username}\n"
+        f"⭐️ Остаток кодов: {user.points}\n"
+        f"🎟 Все коды: {len(user.codes)}\n"
+        f"📅 Дата регистрации: {format_date(str(user.created_at))}"
+    )
+
+    await message.answer(profile_text)
--- main.py
+++ main.py
@@ -8,8 +8,10 @@ from aiogram.client.default import DefaultBotProperties
 from config.config import Config
 
 from handlers.start import router as start_router
+from handlers.profile import router as profile_router
 
 logging.basicConfig(level=logging.INFO)
+
 
 async def main():
     bot = Bot(token=Config.BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
@@ -18,6 +20,7 @@ async def main():
     await bot(DeleteWebhook(drop_pending_updates=True))
 
     dp.include_router(start_router)
+    dp.include_router(profile_router)
 
     await dp.start_polling(bot)
 
