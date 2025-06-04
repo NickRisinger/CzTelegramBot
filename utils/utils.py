44769a1da3524b9a2f3ee54bb9d14b47fbcff304
NickRisinger <boozy044@gmail.com>
2025-06-04 15:22:13 +0300

    fix(gifts): update load gifts

--- utils/utils.py
+++ utils/utils.py
@@ -4,7 +4,8 @@ from datetime import datetime
 from config.config import Config
 
 gtins = []
-products = {}
+
+GIFTS = {}
 
 
 def load_gtins_from_excel():
@@ -24,11 +25,11 @@ def load_products_from_excel():
     workbook = load_workbook(Config.PATH_FILE_PRODUCTS)
     sheet = workbook.active
 
-    products.clear()
+    GIFTS.clear()
 
     for row in sheet.iter_rows(min_row=2, values_only=True):
         if not row[0] is None:
-            products[str(row[0])] = {'name': row[1], 'price': row[2]}
+            GIFTS[str(row[0])] = {'name': row[1], 'price': row[2], 'description': row[3], 'alcohol': bool(row[4]), 'image': row[5]}
 
 
 def format_date(date: str) -> str:
